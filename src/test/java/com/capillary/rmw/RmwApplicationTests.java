fb5640e405e51777786498888ad22a748220bb41
JatinCapillary <jatin@capillarytech.com>
2023-07-20 00:16:03 +0530

    BASIC PROJECT SETUP

---
+++ src/test/java/com/capillary/rmw/RmwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capillary.rmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
